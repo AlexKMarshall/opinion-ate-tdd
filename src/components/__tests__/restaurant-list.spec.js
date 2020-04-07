0cf5ee739b06948ec99de6de8d4ad73f11022f78
Alex Marshall <alex.k.marshall83@gmail.com>
2020-04-07 11:59:02 +0100

    Fix RestaurantList unit tests to use correct component import

--- src/components/__tests__/restaurant-list.spec.js
+++ src/components/__tests__/restaurant-list.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from '@testing-library/react';
-import RestaurantList from '../restaurant-list';
+import {RestaurantList} from '../restaurant-list';
 
 describe('RestaurantList', () => {
   const restaurants = [
