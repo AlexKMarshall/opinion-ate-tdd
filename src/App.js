a436d44b5e56a5bb22e2283a24ac7fbff3b1edc4
Alex Marshall <alex.k.marshall83@gmail.com>
2020-04-07 13:44:00 +0100

    Action loadRestaurants stores restaurants from api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+import store from './store';
 import RestaurantScreen from './components/restaurant-screen';
 
-const App = () => (
-  <div>
-    <RestaurantScreen />
-  </div>
-);
+const App = () => {
+  return (
+    <Provider store={store}>
+      <RestaurantScreen />
+    </Provider>
+  );
+};
 
 export default App;
