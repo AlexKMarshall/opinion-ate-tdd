8daa3d1b048e81a1eeacdae257ef406b4c0c45a0
Alex Marshall <alex.k.marshall83@gmail.com>
2020-04-07 10:48:09 +0100

    Specify app should list restaurants

---
+++ cypress/integration/listing-restaurants.spec.js
@@ -0,0 +1,22 @@
+describe('Listing restaurants', () => {
+  it('shows restaurants from the server', () => {
+    const sushiPlace = 'Sushi Place';
+    const pizzaPlace = 'Pizza Place';
+
+    cy.server({force404: true});
+
+    cy.route({
+      method: 'GET',
+      url:
+        'https://api.outsidein.dev/reKAyRzDG16BpHPiKn88fpRkRsX3OZan/restaurants',
+      response: [
+        {id: 1, name: sushiPlace},
+        {id: 2, name: pizzaPlace},
+      ],
+    });
+
+    cy.visit('/');
+    cy.contains(sushiPlace);
+    cy.contains(pizzaPlace);
+  });
+});
