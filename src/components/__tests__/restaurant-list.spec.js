dea3c00b6ca071b34a986fc111aa002310d1f34a
Alex Marshall <alex.k.marshall83@gmail.com>
2020-04-07 11:44:40 +0100

    Display restaurants in RestaurantList

--- src/components/__tests__/restaurant-list.spec.js
+++ src/components/__tests__/restaurant-list.spec.js
@@ -5,9 +5,30 @@ import RestaurantList from '../restaurant-list';
 describe('RestaurantList', () => {
   it('loads restaurants on first render', () => {
     const loadRestaurants = jest.fn().mockName('loadRestaurants');
+    const restaurants = [];
 
-    render(<RestaurantList loadRestaurants={loadRestaurants} />);
+    render(
+      <RestaurantList
+        loadRestaurants={loadRestaurants}
+        restaurants={restaurants}
+      />,
+    );
 
     expect(loadRestaurants).toHaveBeenCalled();
+  });
+
+  it('displays the restaurants', () => {
+    const noop = () => {};
+    const restaurants = [
+      {id: 1, name: 'Sushi Place'},
+      {id: 2, name: 'Pizza Place'},
+    ];
+
+    const {queryByText} = render(
+      <RestaurantList loadRestaurants={noop} restaurants={restaurants} />,
+    );
+
+    expect(queryByText('Sushi Place')).toBeTruthy();
+    expect(queryByText('Pizza Place')).toBeTruthy();
   });
 });
