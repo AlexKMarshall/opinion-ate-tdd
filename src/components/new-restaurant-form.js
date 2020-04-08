82c6ad84666ac31e79a34e867460320cd26d24f9
Alex Marshall <alex.k.marshall83@gmail.com>
2020-04-09 00:51:39 +0100

    Style the add restaurant input

--- src/components/new-restaurant-form.js
+++ src/components/new-restaurant-form.js
@@ -1,11 +1,22 @@
 import React, {useState} from 'react';
 import {connect} from 'react-redux';
+import {makeStyles} from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Alert from '@material-ui/lab/Alert';
 import {createRestaurant} from '../store/restaurants/actions';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
 export const NewRestaurantForm = ({createRestaurant}) => {
+  const classes = useStyles();
   const [name, setName] = useState('');
   const [validationError, setValidationError] = useState(false);
   const [serverError, setServerError] = useState(false);
@@ -36,21 +47,23 @@ export const NewRestaurantForm = ({createRestaurant}) => {
         </Alert>
       )}
       {validationError && <Alert severity="error">Name is required</Alert>}
-      <TextField
-        value={name}
-        onChange={e => setName(e.target.value)}
-        placeholder="Add Restaurant"
-        fullWidth
-        variant="filled"
-      />
-      <Button
-        type="submit"
-        variant="contained"
-        color="primary"
-        data-testid="new-restaurant-submit-button"
-      >
-        Add
-      </Button>
+      <Box display="flex" className={classes.root}>
+        <TextField
+          value={name}
+          onChange={e => setName(e.target.value)}
+          placeholder="Add Restaurant"
+          fullWidth
+          variant="filled"
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          data-testid="new-restaurant-submit-button"
+        >
+          Add
+        </Button>
+      </Box>
     </form>
   );
 };
